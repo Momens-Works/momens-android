bf2ae04bcd2b1433c7dfc5134be024fb72956582
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-07-02 20:19:51 +0900

    [refactor/#9] Timber 에러메세지 추가

--- app/src/main/java/com/momens/android/core/common/extension/RunCatchingExt.kt
+++ app/src/main/java/com/momens/android/core/common/extension/RunCatchingExt.kt
@@ -1,11 +1,13 @@
 package com.momens.android.core.common.extension
 
+import android.R.id.message
 import timber.log.Timber
 
 
 suspend inline fun <T> Result<T>.onLogFailure(
+    message: String? = null,
     crossinline action: suspend (exception: Throwable) -> Unit
 ): Result<T> = onFailure { e ->
-    Timber.e(e)
+    Timber.e(e, message)
     action(e)
 }
