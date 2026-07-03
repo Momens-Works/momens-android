7438f299f3a16bb069af76aaaa5cbe038f8cbf3c
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-07-04 00:48:37 +0900

    [chore/#13] header 패키지 생성 및 파일 이동

--- app/src/main/java/com/momens/android/core/designsystem/component/header/MomensHeader.kt
+++ app/src/main/java/com/momens/android/core/designsystem/component/header/MomensHeader.kt
@@ -1,4 +1,4 @@
-package com.momens.android.core.designsystem.component
+package com.momens.android.core.designsystem.component.header
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
