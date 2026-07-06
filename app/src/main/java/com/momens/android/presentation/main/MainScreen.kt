3fd325e8f7d5c72f9037b8a698ad09750f8d5e6d
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-07-07 06:43:03 +0900

    [feat/#53] naviagation 적용

--- app/src/main/java/com/momens/android/presentation/main/MainScreen.kt
+++ app/src/main/java/com/momens/android/presentation/main/MainScreen.kt
@@ -1,25 +1,64 @@
 package com.momens.android.presentation.main
 
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.NavHost
+import com.momens.android.presentation.brief.navigation.briefNavGraph
+import com.momens.android.presentation.main.component.MomensMainTabBar
+import com.momens.android.presentation.project.navigation.projectNavGraph
+import com.momens.android.presentation.project.task.detail.navigation.taskDetailNavGraph
+import com.momens.android.presentation.signal.navigation.signalNavGraph
+import com.momens.android.presentation.signin.navigation.signInNavGraph
+import com.momens.android.presentation.splash.navigation.splashNavGraph
 
 @Composable
-fun MainRoute(
-    paddingValues: PaddingValues,
+fun MainScreen(
+    appState: MainAppState = rememberMainAppState(),
 ) {
-    MainScreen(paddingValues = paddingValues)
-}
+    val isBottomBarVisible by appState.isBottomBarVisible.collectAsStateWithLifecycle()
+    val currentTab by appState.currentTab.collectAsStateWithLifecycle()
 
-@Composable
-private fun MainScreen(
-    paddingValues: PaddingValues,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Main",
-        modifier = modifier.padding(paddingValues),
-    )
+    Scaffold(
+        bottomBar = {
+            currentTab?.takeIf { isBottomBarVisible }?.let { tab ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    MomensMainTabBar(
+                        selectedTab = tab,
+                        onTabClick = appState::navigate,
+                    )
+                }
+            }
+        },
+    ) { innerPadding ->
+        NavHost(
+            enterTransition = { EnterTransition.None },
+            exitTransition = { ExitTransition.None },
+            popEnterTransition = { EnterTransition.None },
+            popExitTransition = { ExitTransition.None },
+            navController = appState.navController,
+            startDestination = appState.startDestination,
+        ) {
+            signalNavGraph(paddingValues = innerPadding)
+            briefNavGraph(paddingValues = innerPadding)
+            projectNavGraph(paddingValues = innerPadding)
+            taskDetailNavGraph(paddingValues = innerPadding)
+            signInNavGraph(paddingValues = innerPadding)
+            splashNavGraph(paddingValues = innerPadding)
+        }
+    }
 }
