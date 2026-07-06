5d5a030b1f3d1f4ef89bf7c106f70471135da219
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-07-07 06:42:48 +0900

    [ref/#53] navigationBar 이름 변경

--- app/src/main/java/com/momens/android/presentation/main/type/MainTab.kt
+++ app/src/main/java/com/momens/android/presentation/main/type/MainTab.kt
@@ -2,23 +2,33 @@ package com.momens.android.presentation.main.type
 
 import androidx.annotation.DrawableRes
 import com.momens.android.R
+import com.momens.android.core.common.navigation.Route
+import com.momens.android.presentation.brief.navigation.Brief
+import com.momens.android.presentation.project.navigation.Project
+import com.momens.android.presentation.signal.navigation.Signal
 
-enum class MomensBottomNavigationBarType(
+enum class MainTab(
     val label: String,
     @param:DrawableRes val iconRes: Int,
+    val route: Route,
 ) {
     SIGNAL(
         label = "시그널",
         iconRes = R.drawable.ic_signal,
+        route = Signal,
     ),
 
     BRIEF(
         label = "브리프",
         iconRes = R.drawable.ic_brief,
+        route = Brief,
     ),
 
     TASK(
         label = "태스크",
         iconRes = R.drawable.ic_task,
-    ),
+        route = Project,
+    );
+
+    val routeName: String = route::class.qualifiedName.orEmpty()
 }
