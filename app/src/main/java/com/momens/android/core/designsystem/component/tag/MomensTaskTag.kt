d55bc131ba76102e52d5bf1d557d25764bc4ddd9
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-07-06 19:13:08 +0900

    [feat/#49] Momens TaskTag 구현

---
+++ app/src/main/java/com/momens/android/core/designsystem/component/tag/MomensTaskTag.kt
@@ -0,0 +1,61 @@
+package com.momens.android.core.designsystem.component.tag
+
+import androidx.biometric.R
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.momens.android.core.designsystem.theme.MomensTheme
+import com.momens.android.core.designsystem.type.MomensTaskType
+
+@Composable
+fun MomensTaskTag(
+    type: MomensTaskType,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .clip(RoundedCornerShape(20.dp))
+            .background(MomensTheme.colors.primary10)
+            .border(border = BorderStroke(1.dp, color = MomensTheme.colors.primary50),
+                shape = RoundedCornerShape(20.dp))
+            .padding(horizontal = 8.dp, vertical = 4.dp),
+        horizontalArrangement = Arrangement.spacedBy(2.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            painter = painterResource(type.iconRes),
+            contentDescription = null,
+            tint = MomensTheme.colors.primary100,
+        )
+
+        Text(
+            text = type.label,
+            color = MomensTheme.colors.primary100,
+            style = MomensTheme.typography.captionB11,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun MomensTaskTagPreview() {
+    MomensTheme {
+        MomensTaskTag(
+            type = MomensTaskType.TODO,
+        )
+    }
+}
