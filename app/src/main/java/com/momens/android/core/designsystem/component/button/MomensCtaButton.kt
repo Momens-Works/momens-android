6ab73825b97f592bce45e1a2a77bb40fc063b052
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-07-06 17:12:17 +0900

    [ref/#45]  CTA Button type 방식 변경

--- app/src/main/java/com/momens/android/core/designsystem/component/button/MomensCtaButton.kt
+++ app/src/main/java/com/momens/android/core/designsystem/component/button/MomensCtaButton.kt
@@ -1,18 +1,25 @@
 package com.momens.android.core.designsystem.component.button
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.momens.android.R
 import com.momens.android.core.common.extension.noRippleClickable
 import com.momens.android.core.designsystem.component.type.MomensCtaType
 import com.momens.android.core.designsystem.theme.MomensTheme
@@ -21,16 +28,18 @@ import com.momens.android.core.designsystem.theme.MomensTheme
 fun MomensCtaButton(
     modifier: Modifier = Modifier,
     type: MomensCtaType = MomensCtaType.DEFAULT,
+    enabled: Boolean = true,
     onClick: () -> Unit,
     content: @Composable () -> Unit,
 ) {
-    val backgroundColor = when (type) {
-        MomensCtaType.DEFAULT -> MomensTheme.colors.primary100
-        MomensCtaType.DISABLED -> MomensTheme.colors.gray300
-        MomensCtaType.LOGIN -> MomensTheme.colors.white
+    val backgroundColor = if (!enabled) {
+        MomensTheme.colors.gray300
+    } else {
+        when (type) {
+            MomensCtaType.DEFAULT -> MomensTheme.colors.primary100
+            MomensCtaType.LOGIN -> MomensTheme.colors.white
+        }
     }
-
-    val isEnabled = type != MomensCtaType.DISABLED
 
     Box(
         modifier = modifier
@@ -38,7 +47,7 @@ fun MomensCtaButton(
             .clip(RoundedCornerShape(8.dp))
             .background(backgroundColor)
             .noRippleClickable(
-                enabled = isEnabled,
+                enabled = enabled,
                 onClick = onClick,
             )
             .padding(vertical = 16.dp),
@@ -48,7 +57,7 @@ fun MomensCtaButton(
     }
 }
 
-@Preview
+@Preview(widthDp = 320)
 @Composable
 private fun MomensCtaButtonPreview() {
     MomensTheme {
--- app/src/main/java/com/momens/android/core/designsystem/component/type/MomensCtaType.kt
+++ app/src/main/java/com/momens/android/core/designsystem/component/type/MomensCtaType.kt
@@ -2,6 +2,5 @@ package com.momens.android.core.designsystem.component.type
 
 enum class MomensCtaType {
     DEFAULT,
-    DISABLED,
     LOGIN,
 }
