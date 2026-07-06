01981567e34d2114357566abe6fc1dee6d3b68b8
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-07-06 21:44:12 +0900

    [chore/#49] TaskTag 파일 수정

---
+++ app/src/main/java/com/momens/android/core/designsystem/type/MomensTaskMoreType.kt
@@ -0,0 +1,32 @@
+package com.momens.android.core.designsystem.type
+
+import androidx.annotation.DrawableRes
+import com.momens.android.R
+
+
+enum class MomensTaskMoreType(
+    val label : String,
+    @param: DrawableRes val iconRes: Int
+){
+    TODO(
+        label = "투두",
+        iconRes = R.drawable.ic_todo
+    ),
+    IN_PROGRESS(
+        label = "진행중",
+        iconRes = R.drawable.ic_inprogress
+    ),
+    DONE(
+        label = "완료",
+        iconRes = R.drawable.ic_done
+    ),
+    CANCELED(
+        label = "취소",
+        iconRes = R.drawable.ic_canceled
+    ),
+    BACKLOG(
+        label = "백로그",
+        iconRes = R.drawable.ic_backlog
+    ),
+
+}
