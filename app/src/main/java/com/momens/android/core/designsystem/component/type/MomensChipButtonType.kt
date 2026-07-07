ff7b6ad4bcd4952d8fd79090fd466c6cd36399c2
gahbyu <gahbyu@inha.edu>
2026-07-07 23:32:51 +0900

    [refactor/#21] 코드리뷰 반영

--- app/src/main/java/com/momens/android/core/designsystem/component/type/MomensChipButtonType.kt
+++ app/src/main/java/com/momens/android/core/designsystem/component/type/MomensChipButtonType.kt
@@ -6,37 +6,31 @@ import androidx.compose.ui.text.TextStyle
 import com.momens.android.core.designsystem.theme.MomensTheme
 
 enum class MomensChipButtonType(
-    val textStyle: @Composable () -> TextStyle,
+    val textStyle: @Composable () -> TextStyle = { MomensTheme.typography.captionM10 },
     val textColor: @Composable () -> Color,
     val background: @Composable () -> Color,
 ) {
     PURPLE(
-        textStyle = { MomensTheme.typography.captionM10 },
         textColor = { MomensTheme.colors.white },
         background = { MomensTheme.colors.pointPurple },
     ),
     RED(
-        textStyle = { MomensTheme.typography.captionM10 },
         textColor = { MomensTheme.colors.white },
         background = { MomensTheme.colors.pointRed },
     ),
     BLACK(
-        textStyle = { MomensTheme.typography.captionM10 },
         textColor = { MomensTheme.colors.white },
         background = { MomensTheme.colors.gray800 },
     ),
     WHITE(
-        textStyle = { MomensTheme.typography.captionM10 },
         textColor = { MomensTheme.colors.gray800 },
         background = { MomensTheme.colors.white },
     ),
     YELLOW(
-        textStyle = { MomensTheme.typography.captionM10 },
         textColor = { MomensTheme.colors.white },
         background = { MomensTheme.colors.pointYellow },
     ),
     MINT(
-        textStyle = { MomensTheme.typography.captionM10 },
         textColor = { MomensTheme.colors.white },
         background = { MomensTheme.colors.pointMint },
     ),
