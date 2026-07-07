2502bb2e29e3acd8295d98255ab012c46b651243
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-07-07 14:49:05 +0900

    [chore/#30] data class Immutable 어노테이션 적용

--- app/src/main/java/com/momens/android/core/designsystem/component/type/MomensAccordionItem.kt
+++ app/src/main/java/com/momens/android/core/designsystem/component/type/MomensAccordionItem.kt
@@ -1,5 +1,8 @@
 package com.momens.android.core.designsystem.component.type
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class MomensAccordionItem(
     val title: String,
     val value: String
