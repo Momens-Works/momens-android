54d0e4e0e63af1a919240a9cdd1652b7bc541418
gahbyu <gahbyu@inha.edu>
2026-07-07 23:28:41 +0900

    [refactor/#38] 코드리뷰 반영

--- app/src/main/java/com/momens/android/core/designsystem/component/stateedit/MomensStatusEdit.kt
+++ app/src/main/java/com/momens/android/core/designsystem/component/stateedit/MomensStatusEdit.kt
@@ -1,4 +1,4 @@
-package com.momens.android.core.designsystem.component.componentstatus
+package com.momens.android.core.designsystem.component.stateedit
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
