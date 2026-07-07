4e141fa397d02162b22e992f1d35a3896aee2a3d
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-07-07 23:17:45 +0900

    [chore/#30] ic_next 사이즈 지정(20dp 였음 쌰갈~)

--- app/src/main/java/com/momens/android/core/designsystem/component/accordion/MomensAccordion.kt
+++ app/src/main/java/com/momens/android/core/designsystem/component/accordion/MomensAccordion.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
@@ -133,7 +134,7 @@ private fun MomensAccordionHeader(
         Icon(
             painter = painterResource(R.drawable.ic_next),
             contentDescription = null,
-            modifier = Modifier.rotate(rotation),
+            modifier = Modifier.size(20.dp).rotate(rotation),
             tint = MomensTheme.colors.black,
         )
     }
