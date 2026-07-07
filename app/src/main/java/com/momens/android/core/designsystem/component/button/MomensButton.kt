292b08c11cc08202fcad7bde4698feff9033caff
gahbyu <gahbyu@inha.edu>
2026-07-08 00:03:54 +0900

    [refactor/#21] 코드리뷰 반영

--- app/src/main/java/com/momens/android/core/designsystem/component/button/MomensButton.kt
+++ app/src/main/java/com/momens/android/core/designsystem/component/button/MomensButton.kt
@@ -1,4 +1,4 @@
-package com.momens.android.core.designsystem.component.component.button
+package com.momens.android.core.designsystem.component.button
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
