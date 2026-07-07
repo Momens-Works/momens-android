35f46a009d206f54e64600a46dd01b6e14376d37
gahbyu <gahbyu@inha.edu>
2026-07-08 02:08:55 +0900

    [chore/#21] 불필요한 import 제거

--- app/src/main/java/com/momens/android/core/designsystem/component/button/MomensButton.kt
+++ app/src/main/java/com/momens/android/core/designsystem/component/button/MomensButton.kt
@@ -2,11 +2,9 @@ package com.momens.android.core.designsystem.component.button
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
