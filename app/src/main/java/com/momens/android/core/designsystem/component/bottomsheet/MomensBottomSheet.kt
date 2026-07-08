b20604d328c22a270aba4029b8fab624097c2fe2
gahbyu <gahbyu@inha.edu>
2026-07-09 01:52:00 +0900

    [add/#18] 패키징 추가

--- app/src/main/java/com/momens/android/core/designsystem/component/bottomsheet/MomensBottomSheet.kt
+++ app/src/main/java/com/momens/android/core/designsystem/component/bottomsheet/MomensBottomSheet.kt
@@ -1,4 +1,4 @@
-package com.momens.android.core.designsystem.component
+package com.momens.android.core.designsystem.component.bottomsheet
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -37,8 +37,8 @@ fun MomensBottomSheet(
     onDismiss: () -> Unit,
     modifier: Modifier = Modifier,
     sheetState: SheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
-    dragHandle: @Composable (() -> Unit)? = { MomensDragHandle() },
     containerColor: Color = MomensTheme.colors.white,
+    dragHandle: @Composable (() -> Unit)? = { MomensDragHandle() },
     content: @Composable ColumnScope.() -> Unit,
 ) {
     val shape = RoundedCornerShape(
