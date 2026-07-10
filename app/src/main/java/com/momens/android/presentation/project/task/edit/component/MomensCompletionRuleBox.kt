7d320b9c9dfb1a2a32ce52aa71954e06b8de0477
chldmswll <cuicui633@naver.com>
2026-07-10 15:09:50 +0900

    [ref/#58] MomensCompletionRuleBox.kt 위치를 뷰 컴포넌트로 옮김

--- app/src/main/java/com/momens/android/presentation/project/task/edit/component/MomensCompletionRuleBox.kt
+++ app/src/main/java/com/momens/android/presentation/project/task/edit/component/MomensCompletionRuleBox.kt
@@ -1,4 +1,4 @@
-package com.momens.android.core.designsystem.component.completionrulebox
+package com.momens.android.presentation.project.task.edit.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
