2f6b05ef4ce2e1441e8e35991eb707505e0a97bc
chldmswll <cuicui633@naver.com>
2026-07-12 04:06:01 +0900

    [ref/#58] @Immutable 붙이기

--- app/src/main/java/com/momens/android/presentation/task/edit/model/AssigneeInfo.kt
+++ app/src/main/java/com/momens/android/presentation/task/edit/model/AssigneeInfo.kt
@@ -1,5 +1,8 @@
 package com.momens.android.presentation.task.edit.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class AssigneeInfo(
     val id: String,
     val name: String,
