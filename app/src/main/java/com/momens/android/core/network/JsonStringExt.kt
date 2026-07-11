4635b82cea362298beee60a80192e5abb5590c39
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-07-12 06:21:03 +0900

    [feat/#75] Error Response 응답값 구현

---
+++ app/src/main/java/com/momens/android/core/network/JsonStringExt.kt
@@ -0,0 +1,25 @@
+package com.momens.android.core.network
+
+/**
+ * OkHttp 로그 메시지가 JSON object인지 확인합니다.
+ *
+ * 예시
+ * ```
+ * if (message.isJsonObject()) {
+ *     Timber.d(JSONObject(message).toString(4))
+ * }
+ * ```
+ */
+fun String?.isJsonObject(): Boolean = this?.startsWith("{") == true && this.endsWith("}")
+
+/**
+ * OkHttp 로그 메시지가 JSON array인지 확인합니다.
+ *
+ * 예시
+ * ```
+ * if (message.isJsonArray()) {
+ *     Timber.d(JSONArray(message).toString(4))
+ * }
+ * ```
+ */
+fun String?.isJsonArray(): Boolean = this?.startsWith("[") == true && this.endsWith("]")
