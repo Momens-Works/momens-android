1f0c153b074213cac13f190c7a38f00551c0b1d1
chldmswll <cuicui633@naver.com>
2026-07-12 03:22:57 +0900

    [ref/#58] 미사용 임포트 제거

--- app/src/main/java/com/momens/android/presentation/task/edit/navigation/TaskEditNavigation.kt
+++ app/src/main/java/com/momens/android/presentation/task/edit/navigation/TaskEditNavigation.kt
@@ -5,9 +5,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.momens.android.core.common.navigation.MainTabRoute
 import com.momens.android.core.common.navigation.Route
-import com.momens.android.presentation.task.detail.TaskDetailRoute
 import com.momens.android.presentation.task.edit.TaskEditRoute
 import kotlinx.serialization.Serializable
 
