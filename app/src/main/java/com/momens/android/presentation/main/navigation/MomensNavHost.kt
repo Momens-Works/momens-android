bc65c66f4204beb4210dcdd7c291e33f4afabb16
seungjae708 <choesj365@gmail.com>
2026-07-13 04:25:08 +0900

    [feat/79] Signal에서 Task로의 내비게이션 연동

--- app/src/main/java/com/momens/android/presentation/main/navigation/MomensNavHost.kt
+++ app/src/main/java/com/momens/android/presentation/main/navigation/MomensNavHost.kt
@@ -28,7 +28,10 @@ fun MomensNavHost(
         startDestination = appState.startDestination,
         modifier = modifier,
     ) {
-        signalNavGraph(paddingValues = paddingValues)
+        signalNavGraph(
+            paddingValues = paddingValues,
+            navigateToTask = appState::navigateToTask,
+        )
         briefNavGraph(paddingValues = paddingValues)
         taskNavGraph(paddingValues = paddingValues)
         taskDetailNavGraph(paddingValues = paddingValues)
