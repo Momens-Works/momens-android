1064ad9ebf01134e7cb448b257ea590b87f6420a
chldmswll <cuicui633@naver.com>
2026-07-13 01:25:37 +0900

    [ref/#58] url 추가

--- app/src/main/java/com/momens/android/presentation/task/edit/component/TaskEditAssigneeSearchSection.kt
+++ app/src/main/java/com/momens/android/presentation/task/edit/component/TaskEditAssigneeSearchSection.kt
@@ -48,6 +48,7 @@ fun TaskEditAssigneeSearchSection(
                     text = assignee.name,
                     onClick = { onAssigneeClick(assignee.id) },
                     onDeleteClick = onDeleteClick,
+                    profileImageUrl = assignee.url,
                 )
             }
         }
