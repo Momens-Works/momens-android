e5284efb547aa5a4cb792b6af045423a0f4df5f4
seungjae708 <choesj365@gmail.com>
2026-07-14 01:54:10 +0900

    [feat/#87] TaskDetailTitleSection 컴포넌트 구현

---
+++ app/src/main/java/com/momens/android/presentation/project/taskdetail/component/TaskDetailTitleSection.kt
@@ -0,0 +1,47 @@
+package com.momens.android.presentation.project.taskdetail.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.momens.android.core.designsystem.component.tag.MomensTaskTag
+import com.momens.android.core.designsystem.component.type.MomensStatusEditType
+import com.momens.android.core.designsystem.theme.MomensTheme
+
+@Composable
+fun TaskDetailTitleSection(
+    taskDetailTitle: String,
+    status: MomensStatusEditType,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(9.dp)
+    ) {
+        Text(
+            text = taskDetailTitle,
+            style = MomensTheme.typography.titleB20,
+            color = MomensTheme.colors.black,
+        )
+
+        MomensTaskTag(type = status)
+    }
+}
+
+
+@Preview(showBackground = true, heightDp = 33)
+@Composable
+private fun TaskDetailTitleSectionPreview() {
+    MomensTheme {
+        TaskDetailTitleSection(
+            taskDetailTitle = "일이삼사오육칠팔일이삼사오",
+            status = MomensStatusEditType.TODO,
+        )
+    }
+}
