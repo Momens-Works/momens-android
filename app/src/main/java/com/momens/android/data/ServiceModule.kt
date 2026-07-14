8901498284d28d387fb7471c38afafa2abe739f1
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-07-15 06:55:47 +0900

    [FEAT/MOM-0759] Brief API 의존성 주입 연결

--- app/src/main/java/com/momens/android/data/ServiceModule.kt
+++ app/src/main/java/com/momens/android/data/ServiceModule.kt
@@ -1,5 +1,6 @@
 package com.momens.android.data
 
+import com.momens.android.data.brief.remote.service.BriefService
 import com.momens.android.data.signin.remote.service.SignInService
 import dagger.Module
 import dagger.Provides
@@ -31,4 +32,10 @@ object ServiceModule {
     fun provideSignInService(
         retrofit: Retrofit,
     ): SignInService = retrofit.create(SignInService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideBriefService(
+        retrofit: Retrofit,
+    ): BriefService = retrofit.create(BriefService::class.java)
 }
