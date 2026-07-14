2d88613ffc874e16497dbd4e2953f264c26e94bd
gahbyu <gahbyu@inha.edu>
2026-07-15 06:14:55 +0900

    [FEAT/MOM-0760] Task RemoteDataSource 연동
    TaskRemoteDataSource/TaskRemoteDataSourceImpl에서 TaskService를 호출해 TaskBoardResponse를 가져온다.

--- app/src/main/java/com/momens/android/data/project/task/remote/datasource/TaskRemoteDataSource.kt
+++ app/src/main/java/com/momens/android/data/project/task/remote/datasource/TaskRemoteDataSource.kt
@@ -1,4 +1,7 @@
 package com.momens.android.data.project.task.remote.datasource
 
+import com.momens.android.data.project.task.remote.dto.response.TaskBoardResponse
+
 interface TaskRemoteDataSource {
+    suspend fun getTaskBoard(projectId: String): TaskBoardResponse
 }
--- app/src/main/java/com/momens/android/data/project/task/remote/datasourceimpl/TaskRemoteDataSourceImpl.kt
+++ app/src/main/java/com/momens/android/data/project/task/remote/datasourceimpl/TaskRemoteDataSourceImpl.kt
@@ -1,4 +1,13 @@
 package com.momens.android.data.project.task.remote.datasourceimpl
 
-class TaskRemoteDataSource {
+import com.momens.android.data.project.task.remote.datasource.TaskRemoteDataSource
+import com.momens.android.data.project.task.remote.dto.response.TaskBoardResponse
+import com.momens.android.data.project.task.remote.service.TaskService
+import jakarta.inject.Inject
+
+class TaskRemoteDataSourceImpl @Inject constructor(
+    private val taskService: TaskService,
+) : TaskRemoteDataSource {
+    override suspend fun getTaskBoard(projectId: String): TaskBoardResponse =
+        taskService.getTaskBoard(projectId)
 }
