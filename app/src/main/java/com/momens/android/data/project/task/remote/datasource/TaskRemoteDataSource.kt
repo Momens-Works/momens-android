512778d44efd56d317c14e6243859b424d993002
gahbyu <gahbyu@inha.edu>
2026-07-15 07:59:42 +0900

    [FEAT/MOM-0760] Task 생성 RemoteDataSource 연동
    TaskRemoteDataSource/TaskRemoteDataSourceImpl에 createTask를 추가한다.

--- app/src/main/java/com/momens/android/data/project/task/remote/datasource/TaskRemoteDataSource.kt
+++ app/src/main/java/com/momens/android/data/project/task/remote/datasource/TaskRemoteDataSource.kt
@@ -1,7 +1,14 @@
 package com.momens.android.data.project.task.remote.datasource
 
+import com.momens.android.data.project.task.remote.dto.request.TaskCreateRequest
 import com.momens.android.data.project.task.remote.dto.response.TaskBoardResponse
+import com.momens.android.data.project.task.remote.dto.response.TaskCreateResponse
 
 interface TaskRemoteDataSource {
     suspend fun getTaskBoard(projectId: String): TaskBoardResponse
+
+    suspend fun createTask(
+        projectId: String,
+        request: TaskCreateRequest,
+    ): TaskCreateResponse
 }
--- app/src/main/java/com/momens/android/data/project/task/remote/datasourceimpl/TaskRemoteDataSourceImpl.kt
+++ app/src/main/java/com/momens/android/data/project/task/remote/datasourceimpl/TaskRemoteDataSourceImpl.kt
@@ -1,7 +1,9 @@
 package com.momens.android.data.project.task.remote.datasourceimpl
 
 import com.momens.android.data.project.task.remote.datasource.TaskRemoteDataSource
+import com.momens.android.data.project.task.remote.dto.request.TaskCreateRequest
 import com.momens.android.data.project.task.remote.dto.response.TaskBoardResponse
+import com.momens.android.data.project.task.remote.dto.response.TaskCreateResponse
 import com.momens.android.data.project.task.remote.service.TaskService
 import jakarta.inject.Inject
 
@@ -10,4 +12,10 @@ class TaskRemoteDataSourceImpl @Inject constructor(
 ) : TaskRemoteDataSource {
     override suspend fun getTaskBoard(projectId: String): TaskBoardResponse =
         taskService.getTaskBoard(projectId)
+
+    override suspend fun createTask(
+        projectId: String,
+        request: TaskCreateRequest,
+    ): TaskCreateResponse =
+        taskService.createTask(projectId, request)
 }
