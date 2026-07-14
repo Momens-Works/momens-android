db0c349f49861ccf82a660d40ad494e15d35b50f
gahbyu <gahbyu@inha.edu>
2026-07-15 06:14:45 +0900

    [FEAT/MOM-0760] Task 목록 조회 Service 추가
    TaskService에 GET /api/mobile/projects/{projectId}/tasks 엔드포인트를 추가한다.

--- app/src/main/java/com/momens/android/data/project/task/remote/service/TaskService.kt
+++ app/src/main/java/com/momens/android/data/project/task/remote/service/TaskService.kt
@@ -1,4 +1,12 @@
 package com.momens.android.data.project.task.remote.service
 
+import com.momens.android.data.project.task.remote.dto.response.TaskBoardResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface TaskService {
+    @GET("/api/mobile/projects/{projectId}/tasks")
+    suspend fun getTaskBoard(
+        @Path("projectId") projectId: String,
+    ): TaskBoardResponse
 }
