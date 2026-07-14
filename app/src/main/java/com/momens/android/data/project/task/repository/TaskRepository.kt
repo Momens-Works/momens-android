9e8ad47862667abe06df57c93836e6e964ea0a17
gahbyu <gahbyu@inha.edu>
2026-07-15 06:15:01 +0900

    [FEAT/MOM-0760] Task Repository 연동
    TaskRepository/TaskRepositoryImpl에서 RemoteDataSource 응답을 도메인 모델로 매핑한다.

--- app/src/main/java/com/momens/android/data/project/task/repository/TaskRepository.kt
+++ app/src/main/java/com/momens/android/data/project/task/repository/TaskRepository.kt
@@ -1,4 +1,7 @@
 package com.momens.android.data.project.task.repository
 
+import com.momens.android.data.project.task.model.TaskBoard
+
 interface TaskRepository {
+    suspend fun getTaskBoard(projectId: String): Result<TaskBoard>
 }
--- app/src/main/java/com/momens/android/data/project/task/repositoryimpl/TaskRepositoryImpl.kt
+++ app/src/main/java/com/momens/android/data/project/task/repositoryimpl/TaskRepositoryImpl.kt
@@ -1,4 +1,26 @@
 package com.momens.android.data.project.task.repositoryimpl
 
-class TaskRepositoryImpl {
+import com.momens.android.data.project.task.model.TaskBoard
+import com.momens.android.data.project.task.model.TaskGroup
+import com.momens.android.data.project.task.model.TaskItem
+import com.momens.android.data.project.task.remote.datasource.TaskRemoteDataSource
+import com.momens.android.data.project.task.remote.dto.response.TaskBoardResponse
+import com.momens.android.data.project.task.remote.dto.response.TaskGroupResponse
+import com.momens.android.data.project.task.remote.dto.response.TaskResponse
+import com.momens.android.data.project.task.repository.TaskRepository
+import javax.inject.Inject
+
+class TaskRepositoryImpl @Inject constructor(
+    private val taskRemoteDataSource: TaskRemoteDataSource,
+) : TaskRepository {
+    override suspend fun getTaskBoard(projectId: String): Result<TaskBoard> =
+        runCatching { taskRemoteDataSource.getTaskBoard(projectId).toModel() }
 }
+
+fun TaskBoardResponse.toModel() = TaskBoard(
+    title = title,
+    description = description,
+    groups = groups.map { it.toModel() },
+)
+fun TaskGroupResponse.toModel() = TaskGroup(groupKey, label, count, tasks.map { it.toModel() })
+fun TaskResponse.toModel() = TaskItem(id, title, role, priority, materialCount)
