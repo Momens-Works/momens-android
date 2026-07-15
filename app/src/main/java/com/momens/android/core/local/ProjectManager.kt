9cf31ea86016a82e64520142f18747b6433ed376
PGHoon <116252897+taehoon-An@users.noreply.github.com>
2026-07-15 21:05:39 +0900

    [ref/MOM-0777] swagger 변환에 따른 response 변경 및 Projectmanager 변경

--- app/src/main/java/com/momens/android/core/local/ProjectManager.kt
+++ app/src/main/java/com/momens/android/core/local/ProjectManager.kt
@@ -2,9 +2,14 @@ package com.momens.android.core.local
 
 interface ProjectManager {
 
-    suspend fun saveProjectId(projectId: String?)
+    suspend fun saveProjectContext(
+        projectId: String?,
+        avatarUrl: String?,
+    )
 
     suspend fun getProjectId(): String?
 
-    suspend fun clearProjectId()
+    suspend fun getAvatarUrl(): String?
+
+    suspend fun clearProjectContext()
 }
