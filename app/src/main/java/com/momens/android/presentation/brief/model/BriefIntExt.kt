5eec1d0b70db283df2794388cb33e774fea25fa7
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-07-15 23:10:36 +0900

    [REFACTOR/MOM-0759] Brief 초기 로드 위치 변경

--- app/src/main/java/com/momens/android/presentation/brief/model/BriefIntExt.kt
+++
@@ -1,9 +0,0 @@
-package com.momens.android.presentation.brief.model
-
-fun Int.toProgressFraction(): Float {
-    return if (this > 1) {
-        this / 100f
-    } else {
-        toFloat()
-    }.coerceIn(0f, 1f)
-}
