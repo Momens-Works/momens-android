1dedec48a81093cd441f5a340882d5f324855d84
Hyemin <143622050+hyeminililo@users.noreply.github.com>
2026-07-15 23:10:36 +0900

    [REFACTOR/MOM-0759] Brief 초기 로드 위치 변경

--- app/src/main/java/com/momens/android/presentation/brief/model/BriefStringExt.kt
+++
@@ -1,7 +0,0 @@
-package com.momens.android.presentation.brief.model
-
-fun String.toFilterType(): BriefSignalSummaryFilterType {
-    return BriefSignalSummaryFilterType.entries.firstOrNull { filterType ->
-        filterType.name.equals(this, ignoreCase = true)
-    } ?: BriefSignalSummaryFilterType.ALL
-}
